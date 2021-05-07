f0db29c11b91534c3b05cbc89bb464e3093cb5e3
unknown <admin@PNVL045.pn.com.vn>
2021-05-07 18:19:27 +0700

    Saveeee

--- my-react-router-A/src/App.js
+++ my-react-router-A/src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, NavLink, Link,  Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, NavLink, Link, Switch } from 'react-router-dom';
 import Routers from './component/Routes';
 
 
-const colorWord = {color:'White'}
+const colorWord = { color: 'White' }
 class App extends Component {
   render() {
     return (
       <Router>
-        
+
         <nav>
-        <h2>HELLO</h2>
-        <ul className="nav-link">
-          <li >        
-            <Link to="/" style = {colorWord}>Home</Link></li>
-          <li ><Link to="/About" style = {colorWord} >About</Link></li>
-          <li > <Link to="/Service" style = {colorWord}>Service</Link></li>
-          <li > <Link to="/Contact" style = {colorWord}>Contact</Link></li>
-          <li > <Link to="/AllProduct" style = {colorWord}>AllProduct</Link></li>
-        </ul>
+          <h2>HELLO</h2>
+          <ul className="nav-link">
+            <li className="d-inline"><Link to="/" style={colorWord}>Home</Link></li>
+            <li className="d-inline"><Link to="/About" style={colorWord} >About</Link></li>
+            <li className="d-inline"> <Link to="/Service" style={colorWord}>Service</Link></li>
+            <li className="d-inline"> <Link to="/Contact" style={colorWord}>Contact</Link></li>
+            <li className="d-inline"> <Link to="/AllProduct" style={colorWord}>AllProduct</Link></li>
+          </ul>
         </nav>
         <Switch>
           {this.showContentMenu(Routers)}
@@ -34,7 +33,7 @@ class App extends Component {
     if (Routers.length > 0) {
       result = Routers.map((route, index) => {
         return (
-          
+
           <Route key={index} path={route.path} exact={route.exact} component={route.main} />
         );
       });
